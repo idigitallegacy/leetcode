3c546bb3f747d7730873e147839c553da7443c7a
Michael Makarov <idigitallegacy@yandex.ru>
2025-08-10 03:32:47 +0300

    9 - Palindrome number - [added] stable solution

---
+++ src/main/java/easy/PalindromeNumber9.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class PalindromeNumber9 {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        String str = String.valueOf(x);
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1));
+    }
+}
